38fe1e80d80c73c37e4f7dee74353347c3d93d80
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-06-06 00:04:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+import 'semantic-ui-css/semantic.min.css'
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button, Icon, Label } from 'semantic-ui-react'
+import ProductList from '../pages/ProductList'
+import Categories from './Categories'
+import Navi from './Navi'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Navi />
+            <Categories />
+            <ProductList />
+        </div>
+
+    )
+}
